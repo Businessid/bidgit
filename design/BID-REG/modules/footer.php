174a06ebe5ddf64ebc3de5b8af7d862026b69ead
Businessid <it@businessid.net>
2018-08-11 14:06:08 +0400

    remaining files

---
+++ design/BID-REG/modules/footer.php
@@ -0,0 +1,598 @@
+<script>
+	// Set the date we're counting down to
+	var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+
+	// Update the count down every 1 second
+	var x = setInterval(function() {
+
+	    // Get todays date and time
+	    var now = new Date().getTime();
+	    
+	    // Find the distance between now an the count down date
+	    var distance = countDownDate - now;
+	    
+	    // Time calculations for days, hours, minutes and seconds
+	    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	    
+	    // Output the result in an element with id="demo"
+	    document.getElementById("Count-down-bx").innerHTML =hours + "&nbsp;:"
+	    + minutes + "&nbsp;:" + seconds + "&nbsp;";
+	    
+	    // If the count down is over, write some text 
+	    if (distance < 0) {
+	        clearInterval(x);
+	        document.getElementById("Count-down-bx").innerHTML = "EXPIRED";
+	    }
+	}, 1000);
+</script>
+<script>
+	 function initMap() {
+        var myLatLng = {lat: 25.1972, lng: 55.2744};
+
+        var map = new google.maps.Map(document.getElementById('bx-emp-map'), {
+          zoom: 6,
+          center: myLatLng,
+          styles:[
+				  {
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#ebe3cd"
+				      }
+				    ]
+				  },
+				  {
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#523735"
+				      }
+				    ]
+				  },
+				  {
+				    "elementType": "labels.text.stroke",
+				    "stylers": [
+				      {
+				        "color": "#f5f1e6"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "administrative",
+				    "elementType": "geometry.stroke",
+				    "stylers": [
+				      {
+				        "color": "#c9b2a6"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "administrative.land_parcel",
+				    "elementType": "geometry.stroke",
+				    "stylers": [
+				      {
+				        "color": "#dcd2be"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "administrative.land_parcel",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#ae9e90"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "landscape.natural",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#dfd2ae"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "poi",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#dfd2ae"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "poi",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#93817c"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "poi.park",
+				    "elementType": "geometry.fill",
+				    "stylers": [
+				      {
+				        "color": "#a5b076"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "poi.park",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#447530"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#f5f1e6"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.arterial",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#fdfcf8"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.arterial",
+				    "elementType": "geometry.fill",
+				    "stylers": [
+				      {
+				        "color": "#c0c0c0"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.highway",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#f8c967"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.highway",
+				    "elementType": "geometry.fill",
+				    "stylers": [
+				      {
+				        "color": "#d11d2f"
+				      },
+				      {
+				        "weight": 1.5
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.highway",
+				    "elementType": "geometry.stroke",
+				    "stylers": [
+				      {
+				        "color": "#e9bc62"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.highway.controlled_access",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#e98d58"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.highway.controlled_access",
+				    "elementType": "geometry.stroke",
+				    "stylers": [
+				      {
+				        "color": "#db8555"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.local",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#806b63"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "transit.line",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#dfd2ae"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "transit.line",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#8f7d77"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "transit.line",
+				    "elementType": "labels.text.stroke",
+				    "stylers": [
+				      {
+				        "color": "#ebe3cd"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "transit.station",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#dfd2ae"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "water",
+				    "elementType": "geometry.fill",
+				    "stylers": [
+				      {
+				        "color": "#b9d3c2"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "water",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#92998d"
+				      }
+				    ]
+				  }
+				]
+        });
+
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Hello World!'
+        });
+      }
+
+    </script>
+<!-- <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAeo_cGQozovWzJ-PjvvvCnC0hToHdP2RI&callback=initMap"></script> -->
+<!-- <script src="js/jquery-3.3.1.min.js"></script> -->
+<script
+  src="https://code.jquery.com/jquery-2.2.4.min.js"
+  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+  crossorigin="anonymous"></script> 	
+<script src="bootstrap/js/popper.min.js"></script>
+<script src="js/tether.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Include SmartWizard JavaScript source -->
+<script type="text/javascript" src="js/jquery.smartWizard.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+
+            // Step show event
+            $("#smartwizard").on("showStep", function(e, anchorObject, stepNumber, stepDirection, stepPosition) {
+               //alert("You are on step "+stepNumber+" now");
+               if(stepPosition === 'first'){
+                   $("#prev-btn").addClass('disabled');
+               }else if(stepPosition === 'final'){
+                   $("#next-btn").addClass('disabled');
+               }else{
+                   $("#prev-btn").removeClass('disabled');
+                   $("#next-btn").removeClass('disabled');
+               }
+            });
+
+
+
+
+            // Toolbar extra buttons
+            
+            var btnCancel = $('<button></button>').text('Cancel')
+                                             .addClass('btn cancel-btn-order')
+                                             .on('click', function(){ $('#smartwizard,#smartwizard1').smartWizard("reset"); });
+
+           var btnFinish = $('<button class="save-btn-order" data-toggle="modal" data-target="#mmm"></button>').text('Continue')
+                                             .addClass('btn btn-info')
+                                             .on('click', function(){});                                  
+
+            // Please note enabling option "showStepURLhash" will make navigation conflict for multiple wizard in a page.
+            // so that option is disabling => showStepURLhash: false
+
+            // Smart Wizard 1
+            $('#smartwizard').smartWizard({
+                    selected: 0,
+                    theme: 'arrows',
+                    autoAdjustHeight:'true',
+                    transitionEffect:'fade',
+                    showStepURLhash: false,
+                    toolbarSettings: {toolbarPosition: 'both',
+                                      toolbarExtraButtons: [btnFinish, btnCancel]
+                                    }
+            });
+            $("#smartwizard").on("showStep", function(e, anchorObject, stepNumber, stepDirection) {
+            	 if(stepNumber==3 && stepDirection == 'forward'){
+            	 		$('.sw-btn-group-extra').hide();
+
+            	 }else{
+            	 	$('.sw-btn-group-extra').show();
+            	 }
+            });
+
+            $("#smartwizard1").smartWizard({
+            	 selected: 0,
+                    theme: 'arrows',
+                    autoAdjustHeight:'true',
+                    transitionEffect:'fade',
+                    showStepURLhash: false,
+                    toolbarSettings: {toolbarPosition: 'both',
+                                      toolbarExtraButtons: [btnFinish, btnCancel]
+                                    }
+            });
+
+
+
+        });
+    </script>
+<script src="js/owl.carousel.js"></script>
+<script src="js/zoom-slider.js"></script>
+<script type="text/javascript">
+        $(document).ready( function () {
+            //If your <ul> has the id "glasscase"
+            $('#glasscase').glassCase({ 'thumbsPosition': 'bottom', 'widthDisplay' : 560});
+        });
+    </script>
+<script type="text/javascript">
+	$(document).ready(function(){
+            //-- Click on QUANTITY
+            $(".btn-minus").on("click",function(){
+                var now = $(".section > div > input").val();
+                if ($.isNumeric(now)){
+                    if (parseInt(now) -1 > 0){ now--;}
+                    $(".section > div > input").val(now);
+                }else{
+                    $(".section > div > input").val("1");
+                }
+            })            
+            $(".btn-plus").on("click",function(){
+                var now = $(".section > div > input").val();
+                if ($.isNumeric(now)){
+                    $(".section > div > input").val(parseInt(now)+1);
+                }else{
+                    $(".section > div > input").val("1");
+                }
+            })                        
+        }) 
+</script>    
+<script src="js/jquery.justifiedGallery.min.js"></script>
+<script src="js/jquery.lazy.min.js"></script>
+<script src="js/select2.min.js"></script>
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('.js-example-basic-single').select2();
+     //$('#pages').show();
+});
+</script>
+<script>
+$(document).ready(function() {
+     $('#hot-deals,#best-seller').owlCarousel({
+	    loop:false,
+	    margin:10,
+	    dots:false,
+	    nav:true,
+	    navText: ["<span class='slide-owl icon-arrow-left7'><i class='fa fa-angle-left' aria-hidden='true'></i></span>","<span class='slide-owl icon-arrow-right7'><i class='fa fa-angle-right' aria-hidden='true'></i></span>"],
+	    items:1
+	});  
+     
+});
+
+$(document).ready(function() {
+	// $("#iii").click(function () {
+	// 	alert(1);
+ //    $(".wsmenu-submenu").show();
+	// });
+
+	// $(document).click(function (e) {
+	//     if (!$(e.target).hasClass("iii") 
+	//         && $(e.target).parents(".wsmenu-submenu").length === 0) 
+	//     {
+	//         $(".wsmenu-submenu").hide();
+	//     }
+	// });
+
+	$("#wsmenu-submenu-more").click(function(e){
+    $(".wsmenu-submenu").show();
+     e.stopPropagation();
+});
+
+$(".wsmenu-submenu").click(function(e){
+    e.stopPropagation();
+});
+
+$(document).click(function(){
+    $(".wsmenu-submenu").hide();
+});
+
+
+});
+
+</script>
+<script type="text/javascript">
+  $(document).ready(function() {
+    $("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
+        e.preventDefault();
+        $(this).siblings('a.active').removeClass("active");
+        $(this).addClass("active");
+        var index = $(this).index();
+        $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+        $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+    });
+
+
+    $("#new-address").click(function(){
+    $("#adding-new-address").show();
+	}); 
+
+    $("#reset-pass").click(function(){
+    $(".reset-password-sec").show();
+	}); 
+   	$(".btn-secondary").click(function(){
+    $(".reset-password-sec").hide();
+	}); 
+
+	$("#add-address").click(function(){
+    $("#addnew-address").show();
+	}); 
+   	$("#remove-address").click(function(){
+    $("#addnew-address").hide();
+	});
+
+	$("#qualification-add-btn").click(function(){
+    $("#new-qualification").show();
+	}); 
+   	$("#remove-qualific").click(function(){
+    $("#new-qualification").hide();
+	}); 
+
+	$("#add-address").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#addnew-address").offset().top},
+        'slow');
+	});
+
+	$("#remove-address").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#addnew-address").offset().top},
+        'slow');
+	});
+
+	$("#edit-cv-btn").click(function(){
+    $("#edit-cv-form").show();
+    $("#hero-cv").hide();
+	}); 
+
+	// $("#order-decline").click(function(){
+ //    $(".decline-option-wrap").show();
+ //    $(this).addClass("decline-active");
+	// }); 
+
+});
+</script>
+<script src="js/jquery.nice-select.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+  $('.nice-select').niceSelect();
+});
+</script>
+<script>
+
+$(document).ready(function() {
+	$('.lazy').lazy({
+      effect: "fadeIn",
+      effectTime: 2000,
+      threshold: 0
+    });
+      var boxParentHeight = $('.vert-algn-mdl-cnt').parent().innerHeight();
+      var boxHeigth = $('.vert-algn-mdl-cnt').innerHeight();
+      var maginTop= (boxParentHeight-boxHeigth)/2;
+      $('.vert-algn-mdl-cnt').parent().css('padding-top', maginTop+'px');
+
+
+      $("#bx-tp-js-gallery").justifiedGallery(
+	      {
+			rowHeight : 100,
+			lastRow : 'justify',
+			margins : 1
+	      }
+      );
+      $("#bx-tp-js-gallery-2").justifiedGallery(
+	      {
+			rowHeight : 100,
+			lastRow : 'justify',
+			margins : 1
+	      }
+      )
+      $('#bx-tp-rl-img-glry').owlCarousel({
+	    loop:false,
+	    margin:10,
+	    nav:true,
+	    navText: ["<span class='icon-owl icon-arrow-left7'><i class='fa fa-angle-left' aria-hidden='true'></i></span>","<span class='icon-owl icon-arrow-right7'><i class='fa fa-angle-right' aria-hidden='true'></i></span>"],
+	    items:4,
+	    lazyLoad: true
+	});
+      $('#bx-tp-rl-vdo-glry').owlCarousel({
+	    loop:false,
+	    margin:10,
+	    nav:true,
+	    navText: ["<span class='icon-owl icon-arrow-left7'><i class='fa fa-angle-left' aria-hidden='true'></i></span>","<span class='icon-owl icon-arrow-right7'><i class='fa fa-angle-right' aria-hidden='true'></i></span>"],
+	    items:4,
+	    lazyLoad: true
+	});
+      $('#product-slider-tp-1').owlCarousel({
+	    loop:false,
+	    margin:10,
+	    nav:true,
+	    navText: ["<span class='icon-owl icon-arrow-left7'><i class='fa fa-angle-left' aria-hidden='true'></i></span>","<span class='icon-owl icon-arrow-right7'><i class='fa fa-angle-right' aria-hidden='true'></i></span>"],
+	    items:2,
+	    lazyLoad: true
+	});
+        $('#product-slider-tp-2').owlCarousel({
+	    loop:false,
+	    margin:10,
+	    nav:true,
+	    navText: ["<span class='icon-owl icon-arrow-left7'><i class='fa fa-angle-left' aria-hidden='true'></i></span>","<span class='icon-owl icon-arrow-right7'><i class='fa fa-angle-right' aria-hidden='true'></i></span>"],
+	    items:2,
+	    lazyLoad: true
+	});
+        $('#product-slider-tp-3').owlCarousel({
+	    loop:false,
+	    margin:10,
+	    nav:true,
+	    navText: ["<span class='icon-owl icon-arrow-left7'><i class='fa fa-angle-left' aria-hidden='true'></i></span>","<span class='icon-owl icon-arrow-right7'><i class='fa fa-angle-right' aria-hidden='true'></i></span>"],
+	    items:2,
+	    lazyLoad: true
+	});
+        $('#product-slider-tp-4').owlCarousel({
+	    loop:false,
+	    margin:10,
+	    nav:true,
+	    navText: ["<span class='icon-owl icon-arrow-left7'><i class='fa fa-angle-left' aria-hidden='true'></i></span>","<span class='icon-owl icon-arrow-right7'><i class='fa fa-angle-right' aria-hidden='true'></i></span>"],
+	    items:2,
+	    lazyLoad: true
+	});
+           $('#new-account-box').owlCarousel({
+	    loop:false,
+	    margin:10,
+	    nav:true,
+	    navText: ["<span class='icon-owl icon-arrow-left7'><i class='fa fa-angle-left' aria-hidden='true'></i></span>","<span class='icon-owl icon-arrow-right7'><i class='fa fa-angle-right' aria-hidden='true'></i></span>"],
+	    items:3,
+	    lazyLoad: true
+	});
+});
+</script>
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        @extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h1>Submit a link</h1>
+            <form action="/submit" method="post">
+                @if ($errors->any())
+                    <div class="alert alert-danger" role="alert">
+                        Please fix the following errors
+                    </div>
+                @endif
+
+                {!! csrf_field() !!}
+                <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+                    <label for="title">Title</label>
+                    <input type="text" class="form-control" id="title" name="title" placeholder="Title" value="{{ old('title') }}">
+                    @if($errors->has('title'))
+                        <span class="help-block">{{ $errors->first('title') }}</span>
+                    @endif
+                </div>
+                <div class="form-group{{ $errors->has('url') ? ' has-error' : '' }}">
+                    <label for="url">Url</label>
+                    <input type="text" class="form-control" id="url" name="url" placeholder="URL" value="{{ old('url') }}">
+                    @if($errors->has('url'))
+                        <span class="help-block">{{ $errors->first('url') }}</span>
+                    @endif
+                </div>
+                <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
+                    <label for="description">Description</label>
+                    <textarea class="form-control" id="description" name="description" placeholder="description">{{ old('description') }}</textarea>
+                    @if($errors->has('description'))
+                        <span class="help-block">{{ $errors->first('description') }}</span>
+                    @endif
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+        </div>
+    </div>
+@endsection
+    </body>
+</html>
