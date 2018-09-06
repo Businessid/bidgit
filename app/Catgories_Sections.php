3612279588118aac204ac8ea80af177e8ed05c95
Businessid <it@businessid.net>
2018-09-06 16:05:23 +0400

    ss

---
+++ app/Catgories_Sections.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Catgories_Sections extends Model
+{
+    protected $table = "tbl_categories_sections";
+}
---
+++ app/Company_Hide_Sections.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Company_Hide_Sections extends Model
+{
+    protected $table = 'tbl_company_hide_sections';
+}
