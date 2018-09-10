841fabd1434b38379863c64e286ebb0e73d0d5a3
Businessid <it@businessid.net>
2018-09-10 17:00:17 +0400

    makeModelFileController

--- app/Http/Controllers/ProfileInfoController.php
+++ app/Http/Controllers/ProfileInfoController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Companies;
 use Illuminate\Http\Request;
 
 class ProfileInfoController extends Controller
 {
-    //
+    public function index(){
+
+//        Companies::whare("")
+
+
+
+        return view("front_end.profile.info");
+    }
 }
